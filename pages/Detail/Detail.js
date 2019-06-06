d3d27299f5d4f800678f5ae165285754be854c03
Chen Sihang <1466196675@qq.com>
2019-06-07 01:49:22 +0800

    add comment function

--- pages/Detail/Detail.js
+++ pages/Detail/Detail.js
@@ -1,11 +1,12 @@
 // pages/Detail/Detail.js
+// const moment = require('moment');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    testSameple: {
+    testSample: {
       "data": {
         "id": "5c9ecbbba4a3f52e3195fa68",
         "publisher": {
@@ -16,10 +17,10 @@ Page({
         "title": "帮我洗澡",
         "content": "过来至二634洗澡澡",
         "location": [
-          "中山大学"
+          "中山大学", "中山大学", "中山大学"
         ],
         "tags": [
-          "打游戏"
+          "打游戏", "打游戏", "打游戏"
         ],
         "top_time": 1244123123,
         "status": "overdue",
@@ -60,16 +61,54 @@ Page({
         "like": false
       }
     },
+
+    testComment:{
+    "pagination": {
+      "page": 1,
+      "size": 3,
+      "total": 10
+    },
+    "data": [
+      {
+        "id": "5c9ecbbba4a3f52e3195fa68",
+        "content_id": "5c9ecbbba4a3f52e3195fa68",
+        "content_own": {
+          "id": "5c9ecbbba4a3f52e3195fa68",
+          "nickname": "tp",
+          "avatar": "/images/index_sample.jpg"
+        },
+        "content": "你好骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚骚啊",
+        "user_id": {
+          "id": "5c9ecbbba4a3f52e3195fa68",
+          "nickname": "tp",
+          "avatar": "/images/index_sample.jpg"
+        },
+        "reply_count": 1,
+        "like": false,
+        "like_count": 43,
+        "time": 12414324234
+      }
+    ]
+  },
+    // 日期解析结果
+    publishDate:"",
+    startDate: "",
+    endDate: "",
     // 决定是否显示按钮
     showButtons: false,
-
-
+    isLove: false,
+    isCollected: false,
+    isReplying: false,
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // var util = require("/util/moment.js");
+    // let timestamp = '1546841820000';//时间戳或者别的格式的时间皆可
+    // let time = util.timestampToString(timestamp, 'L');
+    this.setData({isLove : this.data.testSample.data.like});
+    //this.setData({isCollected : this.data.testSample.data.collect});
 
   },
 
@@ -120,5 +159,30 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  // 是否显示其他按钮
+  clickMore: function (e) {
+    if (this.data.showButtons == false){
+      this.setData({showButtons : true});
+    } else{
+      this.setData({showButtons : false});
+    }
+  },
+
+  // 是否喜欢
+  clickLove: function(e) {
+    if (this.data.isLove == true){
+      this.setData({isLove : false});
+    } else{
+      this.setData({isLove : true});
+    }
+  },
+  // 是否收藏
+  clickCollect: function(e) {
+    if (this.data.isCollected == true){
+      this.setData({isCollected : false});
+    } else{
+      this.setData({isCollected : true});
+    }
   }
 })
