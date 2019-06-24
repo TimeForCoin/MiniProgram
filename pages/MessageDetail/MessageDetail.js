96ec1229a79d12b4a77985fa210317af54a72283
Chen Sihang <1466196675@qq.com>
2019-06-25 00:02:39 +0800

    debug and add time token

--- pages/MessageDetail/MessageDetail.js
+++ pages/MessageDetail/MessageDetail.js
@@ -21,30 +21,61 @@ Page({
     noMore: false,
     // 评论和回复内容
     reply_content: "",
-    target_id: "",
+    target_user_id: "",
     taskID:"",
-    status: ""
+    status: "",
+    page:1,
+    session_id:""
   },
 
-  loadMessage: async function(id, str) {
+  // isMore true 用于上拉刷新
+  loadMessage: async function(id, str,isMore) {
+
+    // 用于时间显示
+    moment.locale('zh-cn', {
+      longDateFormat: {
+        l: "YYYY-MM-DD",
+        L: "YYYY-MM-DD HH:mm"
+      }
+    })
+    if(!this.data.isLoading){
+      this.setData({isLoading: true})
+    }
     var res = {}
     if(str === 'detail'){
       console.log('detail')
-      res = await server.request('GET', 'messages/user/' + id)
+      res = await server.request('GET', 'messages/user/' + id, {
+        page: this.data.page,
+        size: 10
+      })
       console.log(res.data)
     } else if(str === 'message'){
-      res = await server.request('GET', 'messages/' + id)
+      res = await server.request('GET', 'messages/' + id, {
+        page: this.data.page,
+        size: 10
+      })
     }
+    console.log(res.statusCode)
     if (res.statusCode == 200) {
       res.data = res.data.data
-      for (let i in res.data.messages) {
-        // 整数时间才进行显示
-        var time = moment(res.data.messages[i].time * 1000).format('L');
-        // 整点判断
-        res.data.messages[i].string_time = moment(res.data.messages[i].time * 1000).format('l');
-        if (time[time.length - 1] == 0 && time[time.length - 2] == 0) {
-          res.data.messages[i].showTime = true;
+      this.setData({
+        isLoading: false
+      })
+      // 没有更多消息
+      if(res.data.messages.length === 0 && isMore){
+        if (isMore) {
+          this.setData({
+            isLoading: false,
+            noMore: true
+          })
+          this.data.page = this.data.page - 1
         }
+      }
+      // 记录会话ID
+      this.data.session_id = res.data.id
+      this.data.status = 'message'
+      this.data.target_user_id = res.data.target_user.id
+      for (let i in res.data.messages) {
         if (res.data.messages[i].user_id === res.data.target_user.id) {
           res.data.messages[i].target_user = res.data.target_user
           res.data.messages[i].self = false
@@ -69,13 +100,45 @@ Page({
         this.data.testSample.data = taskRes.data
       }
       console.log(res.data.messages)
-      this.setData({
-        sessionData: res.data,
-        testMessageDetail: {
-          data: res.data.messages.reverse()
-        },
-        testSample: this.data.testSample,
-      });
+      if(isMore){
+        var arr = []
+        res.data.messages = res.data.messages.reverse()
+        for(var val of res.data.messages){
+          arr.push(val)
+        }
+        for(var val of this.data.testMessageDetail.data){
+          arr.push(val)
+        }
+        // 整点判断
+       for(var i = 0; i < arr.length - 1; i = i + 1){
+         if ((arr[i + 1].time - arr[i].time) > 60 || i === 0) {
+           arr[i].string_time = moment(arr[i].time * 1000).format('L');
+           arr[i].showTime = true;
+         }
+       }
+        
+        this.setData({
+          testMessageDetail: {
+            data: arr
+          },
+        });
+      }else{
+        // 整点判断
+        var arr = res.data.messages.reverse()
+        for (var i = 0; i < arr.length - 1; i = i + 1) {
+          if ((arr[i + 1].time - arr[i].time) > 60 || i === 0) {
+            arr[i].string_time = moment(arr[i].time * 1000).format('L');
+            arr[i].showTime = true;
+          }
+        }
+        this.setData({
+          sessionData: res.data,
+          testMessageDetail: {
+            data: arr
+          },
+          testSample: this.data.testSample,
+        });
+      }
     } else if(str === 'detail'){
       console.log('没有消息')
     }else {
@@ -96,17 +159,18 @@ Page({
         L: "YYYY-MM-DD HH:mm:ss"
       }
     });
-    // 不存在会话id
+    // 判断从哪里穿进来参数
     if(options.status == 'detail'){
       this.data.status = 'detail'
-      this.data.target_id = options.user_id
+      this.data.target_user_id = options.user_id
       this.data.taskID = options.taskID
-      const res = await this.loadMessage(this.data.target_id, options.status)
+      const res = await this.loadMessage(this.data.target_user_id, options.status, false)
     } else if(options.status == 'message'){
       this.data.status = 'message'
       console.log('is message')
       console.log(options.session_id)
-      const res = await this.loadMessage(options.session_id, options.status)
+      this.session_id = options.session_id
+      const res = await this.loadMessage(options.session_id, options.status, false)
     }
     
 
@@ -151,6 +215,13 @@ Page({
    */
   onPullDownRefresh: function() {
 
+  },
+  onPullDownRefresh: function(){
+    if (!this.data.isLoading && !this.data.noMore){
+      this.data.page = this.data.page + 1
+      console.log('page' + this.data.page)
+      this.loadMessage(this.data.session_id, this.data.status, true)
+    }
   },
 
   /**
@@ -202,7 +273,7 @@ Page({
       return;
     }
     // TODO: 在线更新该回复
-    const res = await server.request('POST', 'messages/' + this.data.target_id, {
+    const res = await server.request('POST', 'messages/' + this.data.target_user_id, {
       content: this.data.reply_content,
       about: this.data.status === 'detail' ? this.data.taskID : ""
     })
@@ -210,8 +281,8 @@ Page({
       this.showToast("提交失败", "/images/icons/error.png")
       return
     }
-    this.data.status = 'message'
-    this.loadMessage(res.data.id, this.data.status)
+    this.data.session_id = res.data.id
+    await this.loadMessage(this.data.session_id, this.data.status, false)
     this.showToast("消息提交", "")
     this.setData({
       reply_content: ""
