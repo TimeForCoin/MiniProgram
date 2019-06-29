8196ff63c15879dc71006d27b631bdb76ebf2ea7
Chen Sihang <1466196675@qq.com>
2019-06-30 01:07:17 +0800

    新增左滑右滑，新增对话框高度判断

--- pages/Message/Message.js
+++ pages/Message/Message.js
@@ -19,8 +19,24 @@ Page({
     //判断是否登录
     hasUserInfo: false,
     currentPage: 1,
+    touch:{
+      x: 0,
+      y: 0
+    }
   },
-
+  removeText: function(text) {
+    text = text.replace(/\ +/g, ""); //去掉空格
+    text = text.replace(/[ ]/g, "");    //去掉空格
+    text = text.replace(/[\r\n]/g, ""); //去掉回车换行
+    return text;
+  },
+  // 减少字数
+  reduce: function(text, len){
+    if (text.length > len) {
+      text = text.substr(0, len) + '...'
+    }
+    return text
+  },
   loadMessage: async function(page) {
     this.setData({
       isLoading: true
@@ -70,9 +86,9 @@ Page({
     for (let i in res.data.data) {
       res.data.data[i].string_last_time =
         moment(res.data.data[i].last_message.time * 1000).format('L');
-      if (res.data.data[i].target_user.nickname.length > 14) {
-        res.data.data[i].target_user.nickname = res.data.data[i].target_user.nickname.substr(0, 14) + '...'
-      }
+      res.data.data[i].last_message.content = this.removeText(res.data.data[i].last_message.content) 
+      res.data.data[i].last_message.content = this.reduce(res.data.data[i].last_message.content, 30)
+      res.data.data[i].target_user.nickname = this.reduce(res.data.data[i].target_user.nickname, 14)
       if (res.data.data[i].type === 'chat') {
         this.data.chatMessage.push(res.data.data[i])
       } else {
@@ -187,4 +203,51 @@ Page({
       url: '/pages/MessageDetail/MessageDetail?session_id=' + id + '&status=message',
     })
   },
+  // 开始监听活动
+  touchStart: function (e) {
+    this.setData({
+      touch: {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+      }
+    })
+  },
+  // 结束监听活动
+  touchEnd: async function (e) {
+    let x = e.changedTouches[0].clientX
+    let y = e.changedTouches[0].clientY
+    if (Math.abs(this.data.touch.x - x) > 50) {
+      if (x < this.data.touch.x) {
+        if(this.data.showSystemInfo){
+
+        }else{
+          this.setData({showSystemInfo: true})
+          await this.loadMessage(1)
+        }
+      } else if (this.data.touch.x < x) {
+        if (!this.data.showSystemInfo) {
+
+        } else {
+          this.setData({ showSystemInfo: false })
+          await this.loadMessage(1)
+        }
+      }
+      // 重新加载
+
+    } else { }
+    switch (this.data.currentTab) {
+      case '0':
+        this.loadTaskList(1);
+        break;
+      case '1':
+        this.loadTaskList(1, 'run');
+        break;
+      case '2':
+        this.loadTaskList(1, 'info');
+        break;
+      case '3':
+        this.loadTaskList(1, 'questionnaire');
+        break;
+    }
+  }
 })
